9c5345779f527ffea96905c95e127680f9177a78
Danila S <ek@ek-mail.ru>
2016-12-27 16:34:44 +0300

    WIP: remove joi dependence, now able to pass own joi realization
    it’s important to make this library compatible with jsonapi-joi (they
    have their own Joi).

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 'use strict'; // jshint ignore:line
 let _ = require('lodash');
-let fn = require('./lib/functions');
 
-let JoiSequelize = function(model) {
-  this._joi = {};
-  this._model = model;
-  this._types = {};
-  this._allowNull = [];
-  this.sequelize = {
-    define: require('./lib/define').bind(this)
-  };
-  this.datatypes = require('./lib/datatypes');
-  model(this.sequelize, this.datatypes);
+module.exports = function(ownJoi) {
+	let JoiSequelize = function(model) {
+	  this._joi = {};
+	  this._model = model;
+	  this._types = {};
+	  this._allowNull = [];
+	  this.sequelize = {
+	    define: require('./lib/define')(ownJoi).bind(this)
+	  };
+	  this.datatypes = require('./lib/datatypes');
+	  model(this.sequelize, this.datatypes);
+	};
+
+	let fn = require('./lib/functions')(ownJoi);
+
+	_.merge(JoiSequelize.prototype, fn);
+
+	return JoiSequelize;
 };
-
-_.merge(JoiSequelize.prototype, fn);
-
-
-module.exports = JoiSequelize;
